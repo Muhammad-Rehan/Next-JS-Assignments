e4727fef7d7ef8eb65dba2b8c9d00f381eaa7dcc
Muhammad-Rehan <muhammadrehan320@gmail.com>
2024-11-13 21:32:52 +0500

    Changes removed

--- assignment-06/figma-clone/src/app/page.tsx
+++ assignment-06/figma-clone/src/app/page.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import { Inter } from 'next/font/google'
 
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
